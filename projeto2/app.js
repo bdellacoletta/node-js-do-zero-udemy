a3949858c2434da364fc3d766655034b39549ddd
Branca Scannapieco Della Coletta <brancasdc@gmail.com>
2022-07-30 16:27:10 -0300

    manipulando arquivos com node (fs). Feito 'read, create, save, delete'

--- projeto2/app.js
+++ projeto2/app.js
@@ -2,6 +2,34 @@ const fs = require("fs");
 
 var usuarios = [];
 
-usuarios = JSON.parse(fs.readFileSync("bd.json"));
+function read(item, file) {
+  return item = JSON.parse(fs.readFileSync(file));
+}
+
+function create(array, nome, idade, valor, chave) {
+  array.push({
+    "nome" : nome,
+    "idade" : idade,
+    "valor" : valor,
+    "chave" : chave
+  },);
+}
+
+function save(file, array) {
+  fs.writeFileSync(file, JSON.stringify(array));
+}
+
+function del(array, nome) {
+  for (let index = 0; index < array.length; index++){
+
+    if(nome === array[index].nome){
+      array.splice(index, 1)
+    }
+  }
+}
+
+usuarios = read(usuarios, "bd.json")
+
+del(usuarios, "Bosco");
 
 console.log(usuarios);
