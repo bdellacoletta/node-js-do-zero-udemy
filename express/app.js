19ecc0fedfb94994bbf90c744b3eefe04d8817a4
Branca Scannapieco Della Coletta <brancasdc@gmail.com>
2022-07-30 17:54:26 -0300

    feita a interligação de pastas para incorporar arquivos

--- express/app.js
+++ express/app.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+const public = path.join(__dirname, "public");
+
+app.use("/", express.static(public));
 
 app.get("/", function(req, res){
 
-  res.send("<h1>Hello</h1>");
+  res.sendFile(path.join(public, "filmes.html"));
 
 });
 
-app.get("/user", function(req, res) {
+// criação de nova rota chamada user
+// app.get("/user", function(req, res) {
 
-  res.send("<h1>User</h1>");
+//   res.send("<h1>User</h1>");
 
-});
+// });
 
 app.listen(8080);
