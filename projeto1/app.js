ad883becbf7b178bbb72739f07566233af860eaf
Branca Scannapieco Della Coletta <brancasdc@gmail.com>
2022-07-30 14:49:03 -0300

    criação de função sem parâmetros

--- projeto1/app.js
+++ projeto1/app.js
@@ -2,3 +2,17 @@ var nome = "Branca";
 var idade = 29;
 var chave_pix = 12345678;
 var valor_em_conta = 0;
+
+function mostrar_dados(){
+  console.log("---------------");
+  console.log("Nome: " + nome);
+  console.log("---------------");
+  console.log("Idade: " + idade);
+  console.log("---------------");
+  console.log("Chave pix: " + chave_pix);
+  console.log("---------------");
+  console.log("Valor em conta: " + valor_em_conta);
+  console.log("---------------");
+}
+
+mostrar_dados();
